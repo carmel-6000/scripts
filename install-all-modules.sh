441f635b9ad7632c19a83d321036ffeb5fd8574c
erangep <eran.gep@gmail.com>
2020-02-04 13:30:45 +0200

    updates install-all-modules.sh

--- install-all-modules.sh
+++ install-all-modules.sh
@@ -61,7 +61,8 @@ echo "currDir--->$currDir"
 
 echo "currDir 2? $(dirname "$0")"
 
-currDir="$currDir/../../server"
+#currDir="$currDir/../../server"
+currDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 
 for module in "${!modulesList[@]}"
 do
