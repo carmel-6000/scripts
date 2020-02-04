35e5605f1dab9169c23d75683a8860370ff256f5
erangep <eran.gep@gmail.com>
2020-02-04 14:36:27 +0200

    updates install-all-modules.sh

--- install-all-modules.sh
+++ install-all-modules.sh
@@ -30,9 +30,9 @@ if [ ! -d "$modulesFolder" ];then
 	echo "make sure you run this script from your project root path"
 	echo "and you do have modules installed and then try again"
 	echo
-        exit
-else
-	cd $modulesFolder
+    exit
+#else
+	#cd $modulesFolder
 fi	
 
 if [ ! -d "$modulesFolder/scripts" ];then
