f5567f8fe90e6214cacbf728f8a975198aa7f3b9
maayankoren <nghieuri@gmail.com>
2020-02-26 16:38:15 +0200

    fixing

--- update-samples-json.sh
+++ update-samples-json.sh
@@ -1,13 +1,14 @@
 echo
 echo  "Creating module's samples json (modules/samples/samples_array_data.json)..."
-pwd=$(pwd)
-modulesFolder=$pwd/src/modules
 
-node $modulesFolder/samples/scripts/create-samples-json.js >/dev/null 
+SCRIPT=`realpath $0`
+SCRIPTPATH=`dirname $SCRIPT`
+echo $SCRIPTPATH
+node samples/scripts/create-samples-json.js >/dev/null 
 retVal=$?
 if [ "$retVal" -ne 0 ]; then
 	echo "Error creating samples json (samples/scripts/create-samples-json.js, please check your code..."
 else
-	echo  "${GREEN}OK${NOCOLOR}"
+	echo -e "${GREEN}OK${NOCOLOR}"
 fi
 echo
